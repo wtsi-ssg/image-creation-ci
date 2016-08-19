b7d067e8e287042e1481aa6e2256d43306e50f71
James Beal <James.Beal@sanger.ac.uk>
2016-08-19 09:49:23 +0100

    Try and specialise the sudoers

--- packer/scripts/sudoers.sh
+++ packer/scripts/sudoers.sh
@@ -1,6 +1,12 @@
 #!/bin/bash -eux
 echo hello
 export
-#sed -i -e '/Defaults\s\+env_reset/a Defaults\texempt_group=adm' /etc/sudoers
-#sed -i -e 's/%adm ALL=(ALL) ALL/%adm ALL=NOPASSWD:ALL/g' /etc/sudoers
+cat << EOF > /tmp/edit_sudo_ubuntu.sh
+sed -i -e '/Defaults\s\+env_reset/a Defaults\texempt_group=adm' /etc/sudoers
+sed -i -e 's/%adm ALL=(ALL) ALL/%adm ALL=NOPASSWD:ALL/g' /etc/sudoers
+EOF
+chmod 755 /tmp/edit_sudo_ubuntu.sh
+if [ "$USER" == "ubuntu" ] ; then
+ echo ubuntu | sudo -E -S bash /edit_sudo_ubuntu.sh
+fi
 true
