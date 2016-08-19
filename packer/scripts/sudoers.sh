cb641ee0ee35280dc64fb3d15a19f18a97f5283f
James Beal <James.Beal@sanger.ac.uk>
2016-08-19 09:29:24 +0100

    Inspect env in ci

--- packer/scripts/sudoers.sh
+++ packer/scripts/sudoers.sh
@@ -1,5 +1,6 @@
 #!/bin/bash -eux
-
-export 
-sed -i -e '/Defaults\s\+env_reset/a Defaults\texempt_group=adm' /etc/sudoers
-sed -i -e 's/%adm ALL=(ALL) ALL/%adm ALL=NOPASSWD:ALL/g' /etc/sudoers
+echo hello
+export
+#sed -i -e '/Defaults\s\+env_reset/a Defaults\texempt_group=adm' /etc/sudoers
+#sed -i -e 's/%adm ALL=(ALL) ALL/%adm ALL=NOPASSWD:ALL/g' /etc/sudoers
+true
