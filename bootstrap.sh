0bdcd48fa4200c10e07f89d138d1a50adae54189
James Beal <James.Beal@sanger.ac.uk>
2016-08-19 15:10:05 +0100

    Should work on ubuntu

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
+apt-get install -y ruby rubygems-integration
+
+export
 
 if [ "$IMAGE_USERNAME" == "ubuntu" ] ; then
   apt-get install -y ruby rubygems-integration
+fi
+if [ "$IMAGE_USERNAME" == "centos" ] ; then
+  yum -y install rubygem-bundler ruby
 fi
