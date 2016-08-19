fbd2d7946cf9bb780be9c8f58f98b55cf4951f21
James Beal <James.Beal@sanger.ac.uk>
2016-08-19 15:52:59 +0100

    Fallback to original

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,11 +1,3 @@
 #!/bin/bash
-apt-get install -y ruby rubygems-integration
 
-export
-
-if [ "$IMAGE_USERNAME" == "ubuntu" ] ; then
-  apt-get install -y ruby rubygems-integration
-fi
-if [ "$IMAGE_USERNAME" == "centos" ] ; then
-  yum -y install rubygem-bundler ruby
-fi
+apt-get install -y ruby rubygems-integration 
