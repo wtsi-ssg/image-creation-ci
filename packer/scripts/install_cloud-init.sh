9fc2c09ca6a4729e93b06afe72dd607450e09a08
James Beal <James.Beal@sanger.ac.uk>
2016-08-25 11:51:05 +0100

    Dave knows what he is doing

--- packer/scripts/install_cloud-init.sh
+++ packer/scripts/install_cloud-init.sh
@@ -76,7 +76,7 @@ function cloud-init {
 	   cat <<-EOF > /tmp/cloud_patch
 		--- /etc/cloud/cloud.cfg	2016-08-25 10:23:29.039875922 +0000
 		+++ /tmp/cloud.cfg	2016-08-25 10:29:36.125698275 +0000
-		@@ -25,7 +25,6 @@
+		@@ -25,6 +25,7 @@
 		  - ssh
 		 
 		 cloud_config_modules:
